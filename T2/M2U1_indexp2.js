4f75b58a24fcafd682cbcd0f10913892845d1b9c
daridel99 <darialber@gmail.com>
2022-06-22 21:08:50 -0500

    se anexa el html y js del punto dos siguiendo la logica del DFD, sin css

---
+++ T2/M2U1_indexp2.js
@@ -0,0 +1,56 @@
+function calcular_Mm(){
+	
+	stado=true
+
+    var numero1 = document.getElementById("numero1").value;
+	var numero2 = document.getElementById("numero2").value;
+	var numero3 = document.getElementById("numero3").value;
+	var numero4 = document.getElementById("numero4").value;
+	
+	let entrada = [numero1, numero2, numero3, numero4]
+	console.log(entrada);
+
+	for (var i = 0; i < 4; i++) { //for 1
+	console.log("primer for");
+	var numero=entrada[i]
+	if (numero == '') {
+  	alert("hay celdas vacias");
+	stado=false
+	break;
+	} else {
+  	console.log("No es null");
+	}
+	
+	for (var j = i+1; j < 4; j++) { //for 2
+	console.log("segundo for");
+
+   	if(numero==entrada[j]){
+		
+		alert ("los numeros son iguales entre si");
+		stado=false
+		break;
+		}// if
+
+	} //fin for 1
+	if(stado==false) break;
+	} //fin for 2
+
+	var mayor= entrada[0];
+	var menor= entrada[0];
+	
+	if(stado==true){
+
+	for (var i = 0; i < 4; i++) {
+
+	if(entrada[i]>mayor)	{mayor= entrada[i]}
+	else if(entrada[i]<menor) {menor= entrada[i]}
+	}
+
+	alert ("el numero mayor es: " + mayor + " y el menor es: " + menor);	
+
+	}
+
+	//else{ alert ("ingrese 4 numeros diferentes entre si y/\u00F3 no deje celdas vacia")	}
+
+}
+
